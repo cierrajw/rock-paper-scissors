01b3c121395f1983763d4188578970e7d2eeb150
cierrajw <cw1572@nyu.edu>
2016-07-08 10:03:01 -0400

    Add files via upload

---
+++ rock-paper-scissors/RockPaperScissors/ShortAttentionSpanAI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class ShortAttentionSpanAI : IPlayer
+    {
+        //Random random = new Random();
+        int opponentMove;
+        public int NextMove()
+        {
+                     
+            //gets previous move of opponent and plays the move that would beat them
+            if(opponentMove == 0)
+            {
+                return 1;
+            }
+            else if(opponentMove == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }   
+             
+        }
+
+        public void SaveResult(int myMove, int otherMove)
+        {
+            opponentMove = otherMove;
+
+        }
+
+    }
+}
---
+++ rock-paper-scissors/RockPaperScissors/StubbornAI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors 
+{
+    class StubbornAI : IPlayer
+    {
+        Random random = new Random();
+
+        public int FavoriteMove { get; set; } 
+
+        public int NextMove()
+        {
+            return FavoriteMove;
+        }
+
+        public void SaveResult(int myMove, int otherMove)
+        {
+            //throw new NotImplementedException();C:\Users\WeCanCodeIT\Documents\Visual Studio 2015\Projects\rock-paper-scissors\RockPaperScissors\RandomAI.cs
+        }
+
+        public StubbornAI(int favoriteMove)
+        {
+            FavoriteMove = favoriteMove;
+        }
+
+    }
+}
